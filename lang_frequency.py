27e69ba50749a55c65d4e01ae2135f58861da1f3
manchos <vladm.ru@gmail.com>
2017-01-03 23:12:08 +0300

    the first attempt

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from collections import Counter
 
 
-def load_data(filepath):
-    pass
+def load_utf8text(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, mode='r', encoding='utf8') as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall(r'(\w+)', text)
+    return [pair[0] for pair in Counter(words).most_common(10)]
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Enter the real path to text file with utf8 encoding:')
+    text = load_utf8text(filepath)
+    print('10 most frequency words: %s' % get_most_frequent_words(text))
+
